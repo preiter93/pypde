83fb90645df84ad97fee5c160112a2c688256413
preiter <preiter@gwdg.de>
2021-04-27 12:32:56 +0200

    poisson neumann 2d

--- test/test_poisson.py
+++ test/test_poisson.py
@@ -16,7 +16,7 @@ RTOL = 1e-3 # np.allclose tolerance
 
 class TestPoisson(unittest.TestCase):
     def setUp(self):
-        self.CD = ChebDirichlet(N)
+        pass
 
     def _f(self,x):
         return np.cos(1*np.pi/2*x)
@@ -87,6 +87,72 @@ class TestPoisson(unittest.TestCase):
             .format(norm))
 
         assert np.allclose(uhat,self.solhat, rtol=RTOL)
+
+
+class TestPoissonNeumann(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def _f(self,x):
+        return np.sin(1*np.pi/2*x)
+
+    def usol(self,x):
+        return -np.sin(1*np.pi/2*x)*(1*np.pi/2)**-2
+
+    
+    @classmethod
+    @timeit
+    def setUpClass(cls):
+        print("----------------------------------")
+        print("   Solve Poisson (Neumann 1D)   ")
+        print("----------------------------------")
+        """ 
+        Calculate fftws only once to test solver independently
+        """
+        super(TestPoissonNeumann, cls).setUpClass()
+        cls.N  = N
+        cls.u = SpectralField(cls.N+2, "ChebNeumann")
+        cls.x = cls.u.x
+        cls.f = SpectralField(cls.N+2, "ChebNeumann")
+
+        # Neumann Poisson is singular
+        skipfirst = True
+        skip = slice(1,None) if skipfirst else slice(0,None)
+        cls.skip = skip
+
+        S = cls.u.xs[0].S
+        B = cls.u.xs[0].B(2)@S
+        A = cls.u.xs[0].I()@S
+
+        # -- RHS ---
+        cls.f.v = cls._f(cls,cls.x)
+        cls.f.forward()
+        fhat = cls.f.vhat
+        cls.b = RHSExplicit(f=(B[skip,skip]@fhat[skip]))
+
+        # -- LHS ---
+        A1 = MatrixLHS(A[skip,skip],ndim=1,axis=0, solver="tdma")               
+        cls.A1 = LHSImplicit(A1)            # Exploit bandedness
+
+        # -- Solution ----
+        cls.sol = cls.usol(cls,cls.x)
+        cls.solhat = cls.u.forward_fft(cls.sol)
+
+        print("Initialization finished.")  
+
+    @timeit
+    def test_tdma(self):
+        print("\n ** TDMA Solve **  ")
+
+        # -- Solve ---
+        self.u.vhat[self.skip] = self.A1.solve(self.b.rhs)
+        uhat = self.u.vhat
+        norm = np.linalg.norm( uhat-self.solhat )
+        print(" |pypde - analytical|: {:5.2e}"
+            .format(norm))
+
+        assert np.allclose(uhat,self.solhat, rtol=RTOL)
+
 
 # ---------------------------------------------------------
 #                           2D
@@ -167,6 +233,106 @@ class TestPoisson2D(unittest.TestCase):
 
         # -- Solve ---
         uhat = self.A.solve(self.b.rhs)
+        norm = np.linalg.norm( uhat-self.solhat )
+        print(" |pypde - analytical|: {:5.2e}"
+            .format(norm))
+
+        # import matplotlib.pyplot as plt
+        # u = self.u.backward_fft(uhat)
+        # xx,yy = np.meshgrid(self.x,self.x)
+        # fig = plt.figure()
+        # ax = plt.axes(projection='3d')
+        # ax.plot_surface(xx,yy,u, rstride=1, cstride=1, cmap="viridis",edgecolor="k")
+        # #ax.plot_surface(xx,yy,self.sol)
+        # plt.show()
+
+        assert np.allclose(uhat,self.solhat, rtol=RTOL)
+
+    def eigdecomp(self,A):
+        from pypde.utils import eigen_decomp
+        return eigen_decomp(A)
+
+
+
+ARG = np.pi/2
+class TestPoisson2DNeumann(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def _f(self,xx,yy):
+        return np.sin(ARG*xx)*np.sin(ARG*yy)
+
+    def usol(self,xx,yy):
+        return np.sin(ARG*xx)*np.sin(ARG*yy)*-1/ARG**2/2
+
+    
+    @classmethod
+    @timeit
+    def setUpClass(cls):
+        print("----------------------------------")
+        print("   Solve Poisson (Neumann 2D)   ")
+        print("----------------------------------")
+        """ 
+        Calculate fftws only once to test solver independently
+        """
+        super(TestPoisson2DNeumann, cls).setUpClass()
+        cls.N  = N
+        shape = (N+2,N+2)
+        skipfirst = True
+        skip = slice(1,None) if skipfirst else slice(0,None)
+        cls.skip = skip
+        # -- u
+        cls.u = SpectralField(shape, ("CN","CN"))
+        cls.x,cls.y = cls.u.x,cls.u.y
+        xx, yy = np.meshgrid(cls.x,cls.y,indexing="ij")
+        # -- f
+        cls.f = SpectralField(shape, ("CN","CN"))
+        cls.f.v = cls._f(cls,xx,yy)
+        cls.f.forward()
+
+        # -- Matrices
+        # Dx =  cls.u.xs[0].D(2) 
+        Sx =  cls.u.xs[0].S
+        Bx =  cls.u.xs[0].B(2)@Sx
+        Ax =  cls.u.xs[0].I()@Sx
+
+
+        # -- Eigendecomposition ---
+        Sy =  cls.u.xs[1].S
+        By =  cls.u.xs[1].B(2)@Sy
+        Ay =  cls.u.xs[1].I()@Sy
+        #print(Cy.shape)
+        ByI = np.linalg.inv(By)
+        wy,Qy,Qyi = cls.eigdecomp(cls,Ay.T@ByI.T)
+
+        # -- RHS ---
+        fhat = cls.f.vhat[skip,skip]
+        cls.b = RHSExplicit(f=fhat)
+        cls.b.add_PM(MatrixRHS(Bx[skip,skip],axis=0))
+        cls.b.add_PM(MatrixRHS(Qy.T[skip,skip],axis=1))
+
+        # -- LHS ---
+        Ax = MatrixLHS(A=Bx[skip,skip],ndim=2,axis=0,
+            solver="poisson",lam=wy[skip],C=Ax[skip,skip])
+        Ay = MatrixLHS(A=Qyi[skip,skip].T,ndim=2,axis=1,
+            solver="matmul")
+        cls.A = LHSImplicit(Ax)
+        cls.A.add(Ay)
+
+        # -- Solution ----
+        cls.sol = cls.usol(cls,xx,yy)
+        cls.solhat = cls.u.forward_fft(cls.sol)
+
+        print("Initialization finished.")  
+
+    @timeit
+    def test_solve(self):
+        print("\n ** Numpy Solve **  ")
+
+        # -- Solve ---
+        skip = self.skip
+        self.u.vhat[skip,skip] = self.A.solve(self.b.rhs)
+        uhat = self.u.vhat
         norm = np.linalg.norm( uhat-self.solhat )
         print(" |pypde - analytical|: {:5.2e}"
             .format(norm))
