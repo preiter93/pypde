198c934bc0a6b8b6b780a386ef0487596fb5b920
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2021-06-28 13:02:04 +0200

    removed ticks from anim

--- pypde/plot/anim.py
+++ pypde/plot/anim.py
@@ -103,6 +103,8 @@ def animate_contour(x, y, Z, **kwargs):
     ax.set_xlim((np.amin(x), np.amax(x)))
     ax.set_ylim((np.amin(y), np.amax(y)))
     ax.set_aspect(1)
+    ax.set_xticks([])
+    ax.set_yticks([])
     cvals = np.linspace(np.amin(Z), np.amax(Z), 50)  # set contour value
     x, y = np.meshgrid(x, y, indexing="ij")
     cont = ax.contourf(
